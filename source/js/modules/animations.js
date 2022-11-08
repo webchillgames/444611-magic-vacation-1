4fa311b806ff1926c23dac94cf4f5ef268e653dd
nanawebdev <nanaprettysinger@gmail.com>
2022-11-08 12:58:00 +0300

    1.15. Анимируем фон при переключении экранов

--- source/js/modules/animations.js
+++ source/js/modules/animations.js
@@ -1,7 +1,35 @@
 export default () => {
   const body = document.querySelector(`body`);
+  const prizeMenuLink = document.querySelector(`[data-href="prizes"]`);
+
+  const coverBlock = document.createElement(`div`);
+  coverBlock.classList.add(`cover-block`);
 
   window.onload = () => {
     body.classList.add(`page-loaded`);
+
+    body.appendChild(coverBlock);
   };
+
+  if (window.location.hash === `#story`) {
+    startCoveringBackground();
+  }
+
+  window.addEventListener(`hashchange`, (e) => {
+    if (e.newURL.endsWith(`story`)) {
+      startCoveringBackground();
+    }
+  });
+
+  function startCoveringBackground() {
+    prizeMenuLink.addEventListener(`click`, (e) => {
+      e.preventDefault();
+      coverBlock.classList.add(`active`);
+
+      setTimeout(() => {
+        window.location.hash = `#prizes`;
+      }, 1000);
+
+    });
+  }
 };
